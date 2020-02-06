f3288cb1395834758d25c34859b79b98440db1ac
Rachel Lew <rlew07@gmail.com>
2020-02-05 18:42:44 -0700

    First attempts to get repos from GitHub API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def show
+    user = current_user
+
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      # faraday.headers["GITHUB_TOKEN"] = 'cf29fbde0f252024178eb6e9f9f013be221a7921'
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/users/rlew421/repos")
+
+    json = JSON.parse(response.body)
+    # @repositories = json.map do |repo|
+    #   repo["name"]
+    # end
   end
 
   def new
