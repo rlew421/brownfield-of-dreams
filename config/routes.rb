58ab2199ab0b67baacbd1d41b28c52c95fbdf98c
Rachel Lew <rlew07@gmail.com>
2020-02-09 16:56:26 -0700

    Delete unnecessary route
    Co-Authored-By: Phil DeLong <philjdelong@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
-  get '/auth/github', as: 'github_login'
   get '/auth/github/callback', to: 'sessions#update'
 
   namespace :admin do
