2cdb9e9695066043a75e0d04a8d515a274fc3b89
Rachel Lew <rlew07@gmail.com>
2020-02-11 00:26:20 -0700

    Rollback previous migration, change Friendships table to store but not reference ids

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_055723) do
+ActiveRecord::Schema.define(version: 2020_02_11_072450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
@@ -90,7 +88,6 @@ ActiveRecord::Schema.define(version: 2020_02_11_055723) do
     t.index ["tutorial_id"], name: "index_videos_on_tutorial_id"
   end
 
-  add_foreign_key "friendships", "users"
   add_foreign_key "user_videos", "users"
   add_foreign_key "user_videos", "videos"
 end
