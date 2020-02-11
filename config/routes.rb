0be9b186aafee6251285ee2899d687cd76096d97
Rachel Lew <rlew07@gmail.com>
2020-02-10 22:34:50 -0700

    Add route to create a friendship

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
   get '/auth/github/callback', to: 'sessions#update'
+  post '/friendships', to: 'friendships#create'
 
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
