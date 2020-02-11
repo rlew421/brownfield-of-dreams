2ad3d943063c3fd70c5cf80883ffbbf0a66da210
Rachel Lew <rlew07@gmail.com>
2020-02-10 22:35:11 -0700

    Add id to attr_reader

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,7 +1,8 @@
 class Following
-  attr_reader :login, :html_url
-  
+  attr_reader :login, :html_url, :id
+
   def initialize(attributes = {})
+    @id = attributes[:id]
     @login = attributes[:login]
     @html_url = attributes[:html_url]
   end
