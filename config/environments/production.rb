336c0bbea38d718e59e37a82369d2345ff95334e
Phil DeLong <philjdelong@gmail.com>
2020-02-13 08:58:37 -0700

    Update config to include sendgrid info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+  config.action_mailer.delivery_method = :smtp
+
+  # set Heroku ENV
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'example.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
