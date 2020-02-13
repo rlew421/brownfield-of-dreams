7f8145dafc91c0624bf06d47c93756c78aef066c
Rachel Lew <rlew07@gmail.com>
2020-02-13 08:49:13 -0700

    Add create method

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,21 @@
 class InvitationsController < ApplicationController
   def new
-    
+  end
+
+  def create
+    handle = params[:github_handle]
+    token = current_user.token
+    service = GithubService.new(token)
+    parsed = service.user_follower_show(handle)
+    email = parsed[:email]
+
+    if email != nil
+      InvitationNotifierMailer.inform(current_user, email).deliver_now
+      flash[:success] = "Successfully sent invite!"
+      redirect_to dashboard_path
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+      redirect_to dashboard_path
+    end
   end
 end
